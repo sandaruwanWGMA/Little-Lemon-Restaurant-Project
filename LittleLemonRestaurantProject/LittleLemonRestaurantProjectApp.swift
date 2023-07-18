e58ac45d8bd2e692bbf0103f31851ec9cbc63c11
Molindu Achintha <molinduachintha24@gmail.com>
2023-07-18 05:57:15 +0530

    Initial Commit

---
+++ LittleLemonRestaurantProject/LittleLemonRestaurantProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonRestaurantProjectApp.swift
+//  LittleLemonRestaurantProject
+//
+//  Created by Molindu Achintha on 2023-07-18.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonRestaurantProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
